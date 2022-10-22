a76e217d5fa5f5de35f7d20483146534853eee9d
Chethan B M <35001882+ChethanShetty563@users.noreply.github.com>
2022-10-22 09:46:56 +0530

    Create InsertOP.java

---
+++ Arrays/InsertOP.java
@@ -0,0 +1,14 @@
+	public static int insert(int arr[], int n,int x,int cap, int pos) {
+		
+		
+		if(n == cap) {
+			return n;
+		}
+		
+		int idx = pos-1;
+		for(int i = n - 2;i>=idx;i--) {
+			arr[i+1] = arr[i];
+		}
+		arr[idx] = x;
+		return n+1;
+	}
