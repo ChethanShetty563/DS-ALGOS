994ebe85191959f760f5a360a5387c0683731c20
Chethan B M <35001882+ChethanShetty563@users.noreply.github.com>
2022-11-06 23:46:23 +0530

    Create AllDivisor.java

---
+++ Mathematics/AllDivisor.java
@@ -0,0 +1,14 @@
+void printDivisor(int n) {
+  
+  int i;
+  for(i=1'i*i<n;i++) {
+      if(n%i == 0) {
+      System.out.println(i);
+      }
+    }
+  for(;i>=1;i--) {
+    if(n%i == 0)
+      System.out.println(n/i);
+  }
+      }
+    
