a56359eceba5595409a7e8b0e646a0508371c3de
Chethan B M <35001882+ChethanShetty563@users.noreply.github.com>
2022-11-07 09:05:57 +0530

    Create AbsoluteValue.java

---
+++ Mathematics/AbsoluteValue.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt(); // number of testcases
+        while (T > 0) {
+            int I = sc.nextInt();
+            Solution obj = new Solution();
+            System.out.println(obj.absolute(I));
+
+            T--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int absolute(int n) {
+        // code here
+        if(n>=0 ){
+           return n;
+        }
+        
+        else  {
+            return -n;
+        }
+        
+    }
+}
