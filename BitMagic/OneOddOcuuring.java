2e239e0a3c815543910647ae25f90aea4898da6e
Chethan B M <35001882+ChethanShetty563@users.noreply.github.com>
2022-11-13 22:56:12 +0530

    Update OneOddOcuuring.java

--- BitMagic/OneOddOcuuring.java
+++ BitMagic/OneOddOcuuring.java
@@ -13,3 +13,12 @@ int findOddOccur(int arr[], int n) {
          }
  }
          
+//  Efficient Solution
+int findOdd(int arr[],int n) {
+  int res = arr[0];
+  for(int i=0;i<n;i++) {
+    res =res^arr[i];
+  }
+  return res;
+}
+         
