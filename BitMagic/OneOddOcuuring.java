da51bc703c5e4b4cc7005bc6da6b5642fefc6d5c
Chethan B M <35001882+ChethanShetty563@users.noreply.github.com>
2022-11-13 22:50:37 +0530

    Create OneOddOcuuring.java

---
+++ BitMagic/OneOddOcuuring.java
@@ -0,0 +1,15 @@
+// Naive Solution
+
+int findOddOccur(int arr[], int n) {
+  for(int i=0;i<n;i++) {
+    int count = 0;
+    for(int j=0;j<n;j++) {
+      if(arr[i] == arr[j] {
+        count ++;
+      }
+   }
+         if(count % 2 != 0){
+           return arr[i];
+         }
+ }
+         
