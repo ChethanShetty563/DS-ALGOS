4f583992a57a5c80aea55e1fc68485e56ea5e4d1
Chethan B M <35001882+ChethanShetty563@users.noreply.github.com>
2022-11-13 22:35:50 +0530

    Create IsPowerOFTwo.java

---
+++ BitMagic/IsPowerOFTwo.java
@@ -0,0 +1,15 @@
+// NAive Solution
+
+boolean isPow2(int n) {
+  if(n == 0 ) {
+    return false;
+  }
+  
+  while(n != 1) {
+    if(n%2 != 0) {
+      return false;
+    }
+    n =n /2;
+  }
+  return true;
+}
