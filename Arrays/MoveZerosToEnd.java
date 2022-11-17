33e054f1acf9363c4a7f562332a0768938ee8047
Chethan B M <35001882+ChethanShetty563@users.noreply.github.com>
2022-11-18 00:04:12 +0530

    Update MoveZerosToEnd.java

--- Arrays/MoveZerosToEnd.java
+++ Arrays/MoveZerosToEnd.java
@@ -15,3 +15,22 @@ void moveZeros(int arr[], int n) {
   }
 }
 
+// Another Solution
+//  The time complexity is O(n) but Auxiliry space is also O(n)
+ void pushZerosToEnd(int[] arr, int n) {
+        // code here
+        int res[] = new int[n];
+        int count = 0;
+        for(int i=0;i<n;i++) {
+            if(arr[i] != 0) {
+                res[count++] = arr[i];
+            }
+        }
+        for(int i=count;i<n;i++) {
+            res[i] = 0;
+        }
+         for(int i=0;i<n;i++) {
+            arr[i] = res[i];
+        }
+    }
+
