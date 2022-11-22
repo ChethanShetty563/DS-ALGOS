7ae45ed7b231b5cc077850cbe7a32c7d37f64e1f
Chethan B M <35001882+ChethanShetty563@users.noreply.github.com>
2022-11-22 22:57:48 +0530

    Update BinarySearch.java

--- Searching/BinarySearch.java
+++ Searching/BinarySearch.java
@@ -1,1 +1,23 @@
 
+// Iterative Solution
+
+int bsearch(int arr[], int x, int n) {
+  int low = 0;
+  int high = n - 1;
+  
+  while(low <= high)
+  {
+    int mid = (high + low) / 2;
+    
+    if(arr[mid] == x) {
+      return mid;
+    }
+    else if(arr[mid] > x) {
+      high = mid -1;
+    }
+    else {
+      low = low +1 ;
+    }
+    return -1;
+  }
+  
