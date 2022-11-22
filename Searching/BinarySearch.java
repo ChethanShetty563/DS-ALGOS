dde48c71677dc5d0ba248f581f3c28d9f7b54f10
Chethan B M <35001882+ChethanShetty563@users.noreply.github.com>
2022-11-22 23:04:04 +0530

    Update BinarySearch.java

--- Searching/BinarySearch.java
+++ Searching/BinarySearch.java
@@ -21,3 +21,25 @@ int bsearch(int arr[], int x, int n) {
     return -1;
   }
   
+//   Recursive solution
+  
+  int bsearchr(int arr[], int low, int high, int x ) {
+    if(low >= high) {
+      return -1;
+    }
+    int mid = (low + high) /2;
+    if(arr[mid] == x) {
+      return mid;
+    }
+    
+    else if(arr[mid] >x) {
+      bsaerch(arr,low,mid-1,x);
+    }
+    else {
+      bsaerch(arr,mid+1,high,x);
+    }
+  }
+      
+    
+    
+  
