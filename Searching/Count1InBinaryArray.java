88ca761851f871706357e0808c912438e2838dda
Chethan B M <35001882+ChethanShetty563@users.noreply.github.com>
2022-11-29 06:10:57 +0530

    Update Count1InBinaryArray.java

--- Searching/Count1InBinaryArray.java
+++ Searching/Count1InBinaryArray.java
@@ -12,3 +12,52 @@ public static int countOnes(int arr[], int N){
         }
         return count;
     }
+
+public static int countOnes(int arr[], int N){
+        
+    int low = 0;
+    int high = N ;
+    
+    while(low <high) {
+        
+        int mid = (low + high)/2;
+        
+        if(arr[mid] == 1) {
+            low = mid + 1;
+            
+        }
+        else {
+            high = mid ;
+        }
+        
+        
+    }
+    
+    return high;
+    }
+
+public static int countOnes(int arr[], int N){
+    int index = -1;
+    int low = 0;
+    int high = N ;
+    
+    while(low <high) {
+        
+        int mid = (low + high)/2;
+        
+        if(arr[mid] == 1) {
+                index = mid;
+            low = mid + 1;
+            
+        }
+        else {
+            high = mid ;
+        }
+        
+        
+    }
+    
+    return index + 1;
+    }
+        
+        
