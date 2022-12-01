18d50d60d9ff6fbdfbed8b4e8aa404e2b5c0903a
Chethan B M <35001882+ChethanShetty563@users.noreply.github.com>
2022-12-01 09:25:13 +0530

    Update LeftRotateByD.java

--- Arrays/LeftRotateByD.java
+++ Arrays/LeftRotateByD.java
@@ -35,4 +35,24 @@ void leftRotate(int arr[], int n, int d){
     }
   }
   
+  // The time complexity for this program is O(2n) and space is O(1)
+  
+  void leftRotate(int arr[], int n, int d) {
+    reverse(arr,0,d-1);
+    reverse(arr,d,n-1);
+    reverse(arr,0,n-1);
+  }
+  
+  void reverse(int arr[], int low, int high) {
+    while(low<high) {
+      swap(arr[low], arr[high]);
+      low++;
+      high--;
+    }
+  }
+    
+
+  
+  
+  
 
