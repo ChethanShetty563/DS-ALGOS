ab01b85ef10beda67a9f670bcc9851cadf9e613c
Chethan B M <35001882+ChethanShetty563@users.noreply.github.com>
2022-12-02 11:59:22 +0530

    Update MaxDiffOrder.java

--- Arrays/MaxDiffOrder.java
+++ Arrays/MaxDiffOrder.java
@@ -9,3 +9,14 @@ int maxDiff(int arr[], int n) {
   }
  return res;
  }
+                     
+// The time complexity will be O(n)                     
+int maxDiff(int arr[], int n) {
+  int res = arr[1] - arr[0];
+  int min = arr[0];
+  for(int j=1;j<n;j++) {
+    res = Math.max(res, arr[j] - min);
+    min = Math.min(min,arr[j]);
+  }
+  return res;
+}
