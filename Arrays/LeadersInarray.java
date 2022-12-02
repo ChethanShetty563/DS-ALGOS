6db41e4955a92593d8491e77ef538e69ecbb1c7c
Chethan B M <35001882+ChethanShetty563@users.noreply.github.com>
2022-12-02 11:33:36 +0530

    Create LeadersInarray.java

---
+++ Arrays/LeadersInarray.java
@@ -0,0 +1,17 @@
+// The time complexity is O(n)
+
+void leaders(int arr[], int n) {
+  for(int i=0;i<n;i++) {
+    boolean flag = false;
+    for(int j=i+1;j<n;j++) {
+      if(arr[i] <= arr[j] {
+        flag = true;
+        break;
+      }
+         }
+         if(flag = false){
+           System.out.println(arr[i])
+           }
+         }
+         
+      
