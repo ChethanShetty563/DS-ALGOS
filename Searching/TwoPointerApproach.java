deb590dc4e5a62359856e2c0887babea962c39cc
Chethan B M <35001882+ChethanShetty563@users.noreply.github.com>
2023-01-10 15:52:12 +0530

    Update TwoPointerApproach.java

--- Searching/TwoPointerApproach.java
+++ Searching/TwoPointerApproach.java
@@ -1,6 +1,22 @@
 // Naive Approach
 // Time Complexity O(n^2)
 
+bool ispair(int arr[], int n, int x) {
+  
+  for(int i=0;i<n;i++) {
+    for(int j=0;j<n;j++) {
+      if(arr[i] + arr[j] == x)
+      {
+        return true;
+      }
+    }
+  }
+  return false
+  }
+
+// Naive Approach
+// Time Complexity O(n)
+
 bool ispair(int arr[], int n) {
 int i=0;
 int j = n -1;
